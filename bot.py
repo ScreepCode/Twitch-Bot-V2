ba97dd48acba538e4b71b929f4811643a696b498
Niklas Buse <nicki_bu@gmx.de>
2022-02-14 00:35:18 +0100

    New: command_manager.py -> Ausführen der Module

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from irc.bot import SingleServerIRCBot
 import os
 
+import command_manager
+
 BOT_NAME = "screepcodebot"
 CHANNEL = "screepcode"
 
@@ -16,7 +18,10 @@ class Bot(SingleServerIRCBot):
 
         super().__init__([(self.HOST, self.PORT, f"oauth:{self.TOKEN}")], self.USERNAME, self.USERNAME)
 
-    def send_message(self, message):
+        self.CommandManager = command_manager.CommandManager()
+        self.CommandManager.get_all_callables()
+
+    def send_message(self, message: str):
         self.connection.privmsg(self.CHANNEL, message)
 
     def on_welcome(self, cxn, event):
@@ -31,8 +36,10 @@ class Bot(SingleServerIRCBot):
         print(event)
         tags = {kvpair["key"]: kvpair["value"] for kvpair in event.tags}
         user = {"name": tags["display-name"], "id": tags["user-id"]}
-        message = event.arguments[0]
+        message: str = event.arguments[0]
 
         if user["name"] != self.USERNAME:
-            # cmds.process(self, user, message.lower())
-            pass
+            if message.startswith("!"):
+                response = self.CommandManager.process_cmd(user, message[1:])
+                if response is not None:
+                    self.send_message(response)
---
+++ command_manager.py
@@ -0,0 +1,24 @@
+import sys
+import libs.cmds
+
+
+class CommandManager(object):
+    def __init__(self):
+        self.cmd_list = []
+
+    def get_all_callables(self):
+        self.cmd_list = []
+        for module in sys.modules:
+            if module.startswith("libs.cmds"):
+                if "callables" in dir(sys.modules[module]):
+                    self.cmd_list.append([sys.modules[module], module.title().lower().replace("libs.cmds.", ""), sys.modules[module].callables()])
+
+    def process_cmd(self, user, message):
+        cmd = message.split(" ")[0]
+        for command in self.cmd_list:
+            if cmd in command[2]:
+                resp = getattr(command[0], command[1])()
+                return resp
+
+        else:
+            return None
