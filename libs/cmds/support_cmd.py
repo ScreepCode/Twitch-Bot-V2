33089253bd04bc267e5674dd1952734a7110f872
Niklas Buse <nicki_bu@gmx.de>
2022-02-20 21:13:30 +0100

    Edit: support_cmd functionality

--- libs/cmds/support_cmd.py
+++ libs/cmds/support_cmd.py
@@ -1,7 +1,13 @@
+from libs.data import get_relevant_information
+
 
 def callables():
     return ["support", "spende", "tip"]
 
 
-def support_cmd():
-    return "Support"
+def support_cmd(user, message):
+    json_data = get_relevant_information("support")
+    text = json_data["text"]
+    link = json_data["link"]
+
+    return text.replace("LINK", link)
