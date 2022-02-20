17d3fba9d0b6e6df0df64229722bdf27a523feee
Niklas Buse <nicki_bu@gmx.de>
2022-02-20 20:38:29 +0100

    Add: prime_cmd functionality

--- libs/cmds/prime_cmd.py
+++ libs/cmds/prime_cmd.py
@@ -1,8 +1,12 @@
+from libs.data import get_relevant_information
 
 
 def callables():
     return ["prime", "twitchprime"]
 
 
-def prime_cmd():
-    return "Prime"
+def prime_cmd(user, message):
+    json_data = get_relevant_information("prime")
+    text = json_data["text"]
+
+    return text
