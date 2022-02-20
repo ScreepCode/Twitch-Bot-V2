1dbe7cf1774d6d4772375955862437825a3501db
Niklas Buse <nicki_bu@gmx.de>
2022-02-20 20:48:58 +0100

    Add: stream_plan functionality

--- libs/cmds/stream_plan_cmd.py
+++ libs/cmds/stream_plan_cmd.py
@@ -1,7 +1,12 @@
+from libs.data import get_relevant_information
+
 
 def callables():
     return ["streamplan", "schedule"]
 
 
-def stream_plan_cmd():
-    return "Stream Plan"
+def stream_plan_cmd(user, message):
+    json_data = get_relevant_information("stream-plan")
+    text = json_data["text"]
+
+    return f"{user['name']} {text}"
