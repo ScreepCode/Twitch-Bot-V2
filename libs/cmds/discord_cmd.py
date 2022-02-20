3d08beca7ea3b3da1ea15301b3178c733356f276
Niklas Buse <nicki_bu@gmx.de>
2022-02-20 20:32:40 +0100

    Add: discord_cmd Linkshare

--- libs/cmds/discord_cmd.py
+++ libs/cmds/discord_cmd.py
@@ -1,7 +1,12 @@
+from libs.data import get_relevant_information
+
 
 def callables():
     return ["dc", "discord"]
 
 
-def discord_cmd():
-    return "Discord"
+def discord_cmd(user, message):
+    json_data = get_relevant_information("discord")
+    link = json_data["invite-link"]
+
+    return f"Link zu meinem Community Discord: {link} ^^"
